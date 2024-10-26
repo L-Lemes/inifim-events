3c1906865511c82f16b00a21a59ebe1a9c7e301f
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-25 23:23:08 -0300

    feat(CreateEventUseCase): add use-case

--- src/use-cases/event/create-event-use-case.ts
+++ src/use-cases/event/create-event-use-case.ts
@@ -1,17 +1,16 @@
 import { EventDataValidator } from "../../helpers/event-data-validator.js";
-import { IEvent } from "../../repositories/event-repository.js";
-import { PrismaRepository } from "../../repositories/prisma-repository.js";
+import { EventRepository } from "../../repositories/event-repository.js";
+import { IEvent, TCreateData } from "../../repositories/types-event-repository.js";
 
-type TEventToBeCreated = Omit<IEvent, 'id' | 'createdAt' | 'updatedAt'>;
 
-export class CreateEvent {
-  constructor(private repository: PrismaRepository<IEvent>) {}
+export class CreateEventUseCase {
+  constructor(private repository: EventRepository) {}
   
-  private validate(eventData: TEventToBeCreated): void {
+  private validate(eventData: TCreateData<IEvent>): void {
     EventDataValidator(eventData); 
   }
 
-  async execute(eventData: TEventToBeCreated): Promise<IEvent> {
+  async execute(eventData: TCreateData<IEvent>): Promise<IEvent> {
     this.validate(eventData);
     const newEvent = this.repository.create(eventData);
     return newEvent
