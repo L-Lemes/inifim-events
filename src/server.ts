a903f0021fd6d00eb8105678128c176ba7fbd4b0
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-26 20:39:50 -0300

    feat(server): add routes: /user and /event

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 import express from "express"
 
 import { prisma } from "./prisma.js"
-import { UserRepository } from "./repositories/user-repository.js";
-import { ConfigurationRepository } from "./repositories/configuration-repository.js";
+import { UserRepository } from "./repositories/user/user-repository.js";
+import { ConfigurationRepository } from "./repositories/configuration/configuration-repository.js";
+import { eventRoutes } from "./routes/event.js";
+import { IUser } from "./repositories/user/types-user-reposiotry.js";
+import { userRoutes } from "./routes/user.js";
 
 const app = express()
+
 app.use(express.json())
 
-const data = {
-  overallVolume: 100
-};
-
-const id = '90ab1622-010c-49ec-bdf5-c2033939ed2d'
-
-const userRepository = new ConfigurationRepository(prisma)
-const newUser = await userRepository.update(id, data)
+app.use('/user', userRoutes) 
+app.use('/event', eventRoutes)
 
 app.listen(3333, () => {
   console.log("é o homi n tem jeito")
