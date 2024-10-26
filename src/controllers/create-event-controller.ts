c4592ed4504e8bed4a7386f63c25cd7005e8d061
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-25 23:23:50 -0300

    feat(CreateEventController): add controller

--- src/controllers/create-event-controller.ts
+++ src/controllers/create-event-controller.ts
@@ -1,19 +1,13 @@
-import { IEvent } from "../repositories/event-repository.js";
-import { NextFunction, Request, Response } from "express";
-import { BaseController } from "./controller.js";
-
-type TEventToBeCreated = Omit<IEvent, 'id' | 'createdAt' | 'updatedAt'>;
-
+import { Request, Response, NextFunction } from "express"
+import { IEvent, TCreate, TCreateData } from "../repositories/types-event-repository.js"
 interface TCreateEventUseCase {
-  execute: (eventData: TEventToBeCreated) => Promise<IEvent>
+  execute: (eventData: TCreateData<IEvent>) => Promise<IEvent>
 }
 
-export class EventController extends BaseController {
+export class CreateEventController {
   constructor (
     private createEventUseCase : TCreateEventUseCase
-  ) {
-    super()
-  }
+  ) {}
 
   handle = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const {data} = req.body
--- src/controllers/find-unique-event-controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { BaseController } from "./controller.js";
-
-export class FindUniqueEventController extends BaseController{
-  handle = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    
-  }
-}
