49434ad7392075c74559ee7dd3b882b0aa447753
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-26 20:39:04 -0300

    fix(userRoutes): fix typos

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,15 +1,15 @@
-import { Router } from "express"
-import { CreateEventController } from "../controllers/create-event-controller.js"
+import { NextFunction, Router, Request, Response } from "express"
+import { CreateEventController } from "../controllers/event/create-event-controller.js"
 import { CreateEventUseCase } from "../use-cases/event/create-event-use-case.js"
-import { EventRepository } from "../repositories/event-repository.js"
+import { EventRepository } from "../repositories/event/event-repository.js"
 import { prisma } from "../prisma.js"
 
-const eventRoute = Router()
+const eventRoutes = Router()
 
 const eventRepository = new EventRepository(prisma)
 const createEventUseCase = new CreateEventUseCase(eventRepository)
 const createEventController = new CreateEventController(createEventUseCase)
 
-eventRoute.post('/', createEventController.handle)
+eventRoutes.post('/', (req:Request, res: Response, next: NextFunction) => createEventController.handle(req, res, next))
 
-export { eventRoute }
+export { eventRoutes }
