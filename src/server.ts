81ea14de7d1642d7e351a1514e6e118b29179bdb
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-28 23:19:23 -0300

    feat(server): change pattern to monostate

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,30 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
 
-import { prisma } from "./prisma.js"
-import { UserRepository } from "./repositories/user/user-repository.js";
-import { ConfigurationRepository } from "./repositories/configuration/configuration-repository.js";
 import { eventRoutes } from "./routes/event.js";
-import { IUser } from "./repositories/user/types-user-reposiotry.js";
 import { userRoutes } from "./routes/user.js";
+import { UserRepository } from "./repositories/user/user-repository.js";
+import { LoginUserUseCase } from "./use-cases/user/login-user-use-case.js";
+import { LoginUserControler } from "./controllers/user/login-user-controller.js";
+import { AuthUserController } from "./controllers/user/auth-user-controller.js";
+import { TokenRenewalMiddleware } from "./middlewares/token-renewal-middleware.js";
 
 const app = express()
 
 app.use(express.json())
 
+const userRepository_2 = new UserRepository()
+
+const loginUserUseCase = new LoginUserUseCase(userRepository_2)
+const loginUserControler = new LoginUserControler(loginUserUseCase)
+
+const authUserController = new AuthUserController()
+const tokenRenewalMiddleware = new TokenRenewalMiddleware()
+
+
 app.use('/user', userRoutes) 
-app.use('/event', eventRoutes)
+app.use('/event', tokenRenewalMiddleware.handle, eventRoutes)
+app.post('/login', loginUserControler.handle)
+
 
 app.listen(3333, () => {
   console.log("é o homi n tem jeito")
