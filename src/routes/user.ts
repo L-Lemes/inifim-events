ed74ddeada30d42f476bdedf08b72f52a3d66e09
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-10-28 23:19:45 -0300

    feat(serRoutes): change pattern to monostate

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,17 @@
 import { Request, Response, NextFunction, Router} from "express"
 import { UserRepository } from "../repositories/user/user-repository.js"
-import { CreateUserUseCase } from "../use-cases/user/create-user-use-case.js"
-import { CreateUserController } from "../controllers/user/create-user-controller.js"
+import { RegisterUserUseCase } from "../use-cases/user/register-user-use-case.js"
+import { RegisterUserController } from "../controllers/user/register-user-controller.js"
 import { prisma } from "../prisma.js"
 
 const userRoutes = Router()
 
-const userRepository = new UserRepository(prisma)
-const createUserUseCase = new CreateUserUseCase(userRepository)
-const createUserController = new CreateUserController(createUserUseCase)
+const userRepository = new UserRepository()
+
+userRepository.setPrismaInstance(prisma)
+
+const createUserUseCase = new RegisterUserUseCase(userRepository)
+const createUserController = new RegisterUserController(createUserUseCase)
 
 userRoutes.post('/', (req: Request, res: Response, next: NextFunction) => createUserController.handle(req, res, next))
 
