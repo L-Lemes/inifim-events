023ea4bc5342f357ac988800c8d90ae68ce421c7
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-11-18 09:08:24 -0300

    fix(generateToken)

--- src/utils/generate-jwt.ts
+++ src/utils/generate-jwt.ts
@@ -1,12 +1,12 @@
-import { jwtVerify, SignJWT } from "jose";
+import { SignJWT } from "jose";
 
 export const SECRET_KEY = new TextEncoder().encode(process.env.ACCESS_TOKEN);
 
 export const generateAccessToken = async (userId: string) => {
   return new SignJWT({ id: userId })
-      .setProtectedHeader({ alg: 'HS256' })
-      .setExpirationTime('20m')
-      .sign(SECRET_KEY);
+    .setProtectedHeader({ alg: 'HS256' })
+    .setExpirationTime('2m') // tem 1 mudar pra 15 dps
+    .sign(SECRET_KEY);
 };
 
 
